9b7204d7132b42462829515f332742e26af3d77a
Glen17-S <61369855+Glen17-S@users.noreply.github.com>
2021-08-23 22:36:01 -0600

    Avance
    Avance xd

---
+++ Sistema/Sistema.Datos/DbContextSistema.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sistema.Datos.Mapping.Almacen;
+using Sistema.Entidades.Almacen;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema.Datos
+{
+    public class DbContextSistema : DbContext
+    {
+        //Para exponer la coleccion de todas las categorias
+        public DbSet<Categoria> Categorias { get; set; }
+
+        //Contructor de conexion
+        public DbContextSistema(DbContextOptions<DbContextSistema> options) : base (options)
+        {
+
+        }
+        
+        //Metodo
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CategoriaMap());
+        }
+    }
+}
---
+++ Sistema/Sistema.Datos/Mapping/Almacen/CategoriaMap.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sistema.Entidades.Almacen;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema.Datos.Mapping.Almacen
+{
+    // los : es heredar del EntityFramework del tipo categoria
+    public class CategoriaMap : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            //Para mapear la tabla Categoria con la llave primaria
+            builder.ToTable("categoria")
+                 .HasKey(c => c.idCategoria);
+
+            builder.Property(c => c.Nombre)
+                .HasMaxLength(50);
+
+            builder.Property(c => c.Descripcion)
+                .HasMaxLength(256);
+        }
+    }
+}
